472dc73acf87708b62f5713a534cfa6e434a91af
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-03-22 12:51:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let openBtn=document.querySelector("#openModal")
+let modal=document.querySelector(".modal")
+let closeBtn=document.querySelector(".close-modal")
+openBtn.addEventListener("click",display)
+function display(){
+	modal.style.display="block";
+}
+closeBtn.addEventListener("click",close)
+function close(e){
+	modal.style.display="none";
+}
+window.addEventListener("click",close2)
+function close2(e){
+	if(e.target===modal)
+	{
+		modal.style.display="none";
+	}
+}
